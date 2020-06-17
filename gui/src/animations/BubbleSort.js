67ad54c096540bac47d4765b91e7ccbea85a9374
Luis Preciado <luisapreciado99@gmail.com>
2020-06-17 12:28:23 -0500

    new modal

--- gui/src/animations/BubbleSort.js
+++ gui/src/animations/BubbleSort.js
@@ -1,22 +1,23 @@
 import { bubbleSort } from "../algorithms/BubbleSort";
-import {PRIMARY_COLOR, ANIMATION_SPEED_MS, SECONDARY_COLOR} from "../utility/constants"
+import * as cts from "../utility/constants";
 
 export const bubbleSortAnimation = async (array, setIsDisabled) => {
-    const animation = bubbleSort(array);
-    const arrayBars = document.getElementsByClassName("array-bar");
-    for (let i = 0; i < animation.length; i++) {
-      const [prev, next, prevHeight, nextHeight] = animation[i];
+  const animation = [];
+  bubbleSort(array, animation);
+  const arrayBars = document.getElementsByClassName("array-bar");
+  for (let i = 0; i < animation.length; i++) {
+    const [prev, next, prevHeight, nextHeight] = animation[i];
 
-      setTimeout(() => {
-        arrayBars[prev].style.backgroundColor = PRIMARY_COLOR;
-        arrayBars[next].style.backgroundColor = SECONDARY_COLOR;
-        if (nextHeight || prevHeight) {
-          arrayBars[prev].style.height = `${prevHeight}px`;
-          arrayBars[next].style.height = `${nextHeight}px`;
-        }
-        if (i === animation.length - 1) {
-          setIsDisabled(false);
-        }
-      }, (i * ANIMATION_SPEED_MS) / 10);
-    }
-  };
+    setTimeout(() => {
+      arrayBars[prev].style.backgroundColor = cts.PRIMARY_COLOR;
+      arrayBars[next].style.backgroundColor = cts.PRIMARY_COLOR;
+      if (nextHeight || prevHeight) {
+        arrayBars[prev].style.height = `${prevHeight}px`;
+        arrayBars[next].style.height = `${nextHeight}px`;
+      }
+      if (i === animation.length - 1) {
+        setIsDisabled(false);
+      }
+    }, (i * cts.ANIMATION_SPEED_MS) / 10);
+  }
+};
