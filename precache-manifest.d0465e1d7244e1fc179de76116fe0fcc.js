2afe69eedc7a2e8f932cab57dcb8e3bdbee84d5a
Luis Preciado <luisapreciado99@gmail.com>
2020-06-24 10:47:24 -0500

    Updates

--- precache-manifest.d0465e1d7244e1fc179de76116fe0fcc.js
+++ precache-manifest.d0465e1d7244e1fc179de76116fe0fcc.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "7d4442099680538034db97c391c1d67b",
+    "revision": "e355eba8e88982f60be9275523d33ba2",
     "url": "/Sorting-Visualizer/index.html"
   },
   {
-    "revision": "ee1f5a37a4e818317f29",
+    "revision": "8541981b87a89d8dc791",
     "url": "/Sorting-Visualizer/static/css/main.307b1dda.chunk.css"
   },
   {
-    "revision": "f95efc91fdce37534872",
-    "url": "/Sorting-Visualizer/static/js/2.9a0e2d5e.chunk.js"
+    "revision": "3e7c52d783f4be70c572",
+    "url": "/Sorting-Visualizer/static/js/2.9227db9e.chunk.js"
   },
   {
     "revision": "0749163b59fbee32225059cb60c18af6",
-    "url": "/Sorting-Visualizer/static/js/2.9a0e2d5e.chunk.js.LICENSE.txt"
+    "url": "/Sorting-Visualizer/static/js/2.9227db9e.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "ee1f5a37a4e818317f29",
-    "url": "/Sorting-Visualizer/static/js/main.86912d05.chunk.js"
+    "revision": "8541981b87a89d8dc791",
+    "url": "/Sorting-Visualizer/static/js/main.e21a944d.chunk.js"
   },
   {
     "revision": "2a1e0847574d9a9782b1",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Sorting-Visualizer/precache-manifest.250d65299e2138dc94471f080fa1416b.js"
+  "/Sorting-Visualizer/precache-manifest.d0465e1d7244e1fc179de76116fe0fcc.js"
 );
 
 self.addEventListener('message', (event) => {
