934b5bac37fbed734275c5bb718ad63030e92f02
Luis Preciado <luisapreciado99@gmail.com>
2020-07-23 06:53:41 -0500

    test

--- gui/src/utility/index.js
+++ gui/src/utility/index.js
@@ -3,6 +3,7 @@ export * from "./constants"
 const getRandomInt = (max) => {
   return Math.floor(Math.random() * Math.floor(max)) + 1;
 };
+
 export const generateRandomArray = (length) => {
   let array = [];
   for (let i = 0; i < length; i++) {
